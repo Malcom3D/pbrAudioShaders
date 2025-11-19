fce3a9ef29bfcae8667c1db1a854aaa5732536bc
Malcom3D <Malcom3D@users.noreply.github.com>
2025-11-19 17:06:14 +0100

    fix(dsp_faust): unsensed repo fix

---
+++ src/old_git_faust/build/CMakeFiles/dsp_faust.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsp_faust.dir/src/dsp-faust/DspFaust.cpp.o"
+  "CMakeFiles/dsp_faust.dir/src/dsp-faust/DspFaust.cpp.o.d"
+  "CMakeFiles/dsp_faust.dir/src/dsp_faust.cpp.o"
+  "CMakeFiles/dsp_faust.dir/src/dsp_faust.cpp.o.d"
+  "dsp_faust.cpython-311-x86_64-linux-gnu.so"
+  "dsp_faust.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dsp_faust.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
