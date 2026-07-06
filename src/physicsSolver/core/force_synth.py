0f869a1597630724cdec2f1495722157d0e34ee4
Malcom3D <Malcom3D@users.noreply.github.com>
2026-07-06 15:58:12 +0200

    feat(pbrAudioBlender): new output properties

--- src/physicsSolver/core/force_synth.py
+++ src/physicsSolver/core/force_synth.py
@@ -29,7 +29,8 @@ from ..core.entity_manager import EntityManager
 from ..lib.force_data import ContactType
 from ..lib.hertzian_contact import HertzianContact
 #from ..lib.audio_forces_denoiser import AudioForcesDenoiser
-from postProcess import AudioForcesDenoiser
+from postProcess.lib.audio_forces_denoiser import AudioForcesDenoiser
+#from postProcess import AudioForcesDenoiser
 
 @dataclass
 class ForceSynth:
