b5df83185db8fe54e5c25cbbcab869e19a9ae0de
Malcom3D <Malcom3D@users.noreply.github.com>
2026-07-13 18:13:54 +0200

    feat(pbrAudioBlender): new ellipsoidalProxy module published

--- src/physicsSolver/utils/config.py
+++ src/physicsSolver/utils/config.py
@@ -108,7 +108,7 @@ class ObjectConfig:
     obj_path: str
     pose_path: str
     static: bool
-    proxy: Union[bool, int] = False # 0 = octahedron, 1 = icosahedron for < blender.proxy_size_threshold, 2,3,4 for low,mid,hi manual selection and icosahedron subdivision
+    proxy_type: Union[bool, int] = False # 0 = octahedron, 1 = icosahedron for < blender.proxy_size_threshold, 2,3,4 for low,mid,hi manual selection and icosahedron subdivision
     ground: bool = False
     resonance: bool = False
     resonance_modes: int = 10
