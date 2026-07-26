b18af88f2ae04bdffe6ef23532a816d0e1e3a5c2
Malcom3D <Malcom3D@users.noreply.github.com>
2026-07-26 15:16:57 +0200

    release(all): pbrAudio 0.2.4 beta

--- src/rigidBody/core/modal_player.py
+++ src/rigidBody/core/modal_player.py
@@ -351,8 +351,8 @@ class ModalPlayer:
         if os.path.exists(f"{sound_path}/{obj_name}_sliding_sound.raw"):
             sliding_sound += np.fromfile(f"{sound_path}/{obj_name}_sliding_sound.raw", dtype=np.float32)
         if os.path.exists(f"{sound_path}/{obj_name}_scraping_sound.raw"):
-            scraping_sound += += np.fromfile(f"{sound_path}/{obj_name}_scraping_sound.raw", dtype=np.float32)
+            scraping_sound += np.fromfile(f"{sound_path}/{obj_name}_scraping_sound.raw", dtype=np.float32)
         if os.path.exists(f"{sound_path}/{obj_name}_rolling_sound.raw"):
-            rolling_sound += np.fromfile(f"{"{sound_path}/{obj_name}_rolling_sound.raw", dtype=np.float32)
+            rolling_sound += np.fromfile(f"{sound_path}/{obj_name}_rolling_sound.raw", dtype=np.float32)
         
         return sliding_sound, scraping_sound, rolling_sound
