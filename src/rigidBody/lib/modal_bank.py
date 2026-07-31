6105202fa7a9500e321432407ba01609a23b7417
Malcom3D <Malcom3D@users.noreply.github.com>
2026-07-31 19:15:38 +0200

    release(all): pbrAudio 0.2.4 beta

--- src/rigidBody/lib/modal_bank.py
+++ src/rigidBody/lib/modal_bank.py
@@ -58,9 +58,9 @@ def process_all_mode(output, num_modes, c, s, g, u1, u2, excitation):
         # Ensure we're using float32 arithmetic
         u1_new = c[i] * u1[i] - s[i] * u2[i] + g[i] * excitation
         u2_new = s[i] * u1[i] + c[i] * u2[i]
-        u1[i] = np.float32(u1_new)
-        u2[i] = np.float32(u2_new)
-        output += np.float32(u2_new)
+        u1[i] = u1_new.astype(np.float32)
+        u2[i] = u2_new.astype(np.float32)
+        output += u2_new.astype(np.float32)
     return output, u1, u2
 
 class ModalBank:
