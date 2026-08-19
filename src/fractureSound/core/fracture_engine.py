9055285dd4a8b3d6c0e90e510919e13dd4807af8
Malcom3D <Malcom3D@users.noreply.github.com>
2026-08-19 16:46:25 +0200

    release(all): pbrAudio 0.2.4.1 beta

--- src/fractureSound/core/fracture_engine.py
+++ src/fractureSound/core/fracture_engine.py
@@ -25,10 +25,10 @@ from dask import delayed, compute
 from pbrAudioCommon import EntityManager
 from pbrAudioCommon import debug_print, set_debug, set_debug_prefix
 
-#from physicsSolver import TrajectoryData, CollisionData, ForceDataSequence
-from physicsSolver.lib.force_data import ForceDataSequence
-from physicsSolver.lib.collision_data import CollisionData
-from physicsSolver.lib.trajectory_data import TrajectoryData
+from physicsSolver import TrajectoryData, CollisionData, ForceDataSequence
+#from physicsSolver.lib.force_data import ForceDataSequence
+#from physicsSolver.lib.collision_data import CollisionData
+#from physicsSolver.lib.trajectory_data import TrajectoryData
 
 from ..lib.fracture_data import FractureEvent, FractureType
 from ..lib.fracture_detector import FractureDetector
