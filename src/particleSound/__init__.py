d376f727bd404c846a08a975c69660444432c32f
Malcom3D <Malcom3D@users.noreply.github.com>
2026-09-08 17:44:01 +0200

    release(all): pbrAudio 0.2.4.1 beta

--- src/particleSound/__init__.py
+++ src/particleSound/__init__.py
@@ -26,11 +26,11 @@ import numpy as np
 decimals = 18
 np.set_printoptions(precision=decimals, floatmode='fixed', threshold=np.inf)
 
-from .core.particles_solver import ParticlesSolver
+from .core.particle_trajectory_solver import ParticlesTrajectorySolver
 from .lib.particle_trajectory_data import ParticleTrajectoryData
 
 
 __all__ = [
-    'ParticlesSolver',
+    'ParticlesTrajectorySolver',
     'ParticleTrajectoryData'
 ]
